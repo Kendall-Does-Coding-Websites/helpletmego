0ec1e7aaecdb051b5980229c9228a430e6597bb0
Kendall Pinto <kendalldoescoding@gmail.com>
2022-02-01 21:32:55 +0530

    temp js template

---
+++ 2/index.js
@@ -0,0 +1,34 @@
+
+const little_saint_nick = lyrics.find(
+  (x) => x.name === "little saint nick"
+).lyrics;
+
+const little_saint_nick__container =
+  document.getElementsByClassName("lyrics")[0];
+
+little_saint_nick.map((item) => {
+  const p = document.createElement("p");
+  const textNode = document.createTextNode(item);
+  p.appendChild(textNode);
+  return little_saint_nick__container.appendChild(p);
+});
+
+const slider_little_saint_nick = document.querySelector(".slider");
+
+const toggle_little_saint_nick = document.querySelector(".round");
+toggle_little_saint_nick.addEventListener("click", modeSwitch);
+
+let isLight_little_saint_nick = true;
+
+function modeSwitch() {
+  isLight_little_saint_nick = !isLight_little_saint_nick;
+
+  isLight_little_saint_nick
+    ? (slider_little_saint_nick.style.backgroundImage =
+        "url('../../images/day.png')")
+    : (slider_little_saint_nick.style.backgroundImage =
+        "url('../../images/night.png')");
+
+  const rootElement = document.body;
+  rootElement.classList.toggle("darkMode");
+}
